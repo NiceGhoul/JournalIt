87e9f0d0f3356079b35aaefb7aa747f5fcbda79f
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2024-11-26 21:50:37 +0700

    homepage

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,18 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'showHomePage'])->name('homePage');
-Route::get('/ProfilePage', [UserController::class, 'profile'])->name('ProfilePage')->middleware('auth');
-Route::patch('/to-do-lists/{id}/update-progress', [ToDoListController::class, 'updateProgress'])->name('updateProgress');
-Route::post('/logout', [UserController::class, 'accountLogout'])->name('logout');
 Route::get('/login', [UserController::class, 'getLoginPage'])->name('login');
 Route::post('/login', [UserController::class, 'accountLogin'])->name('loginAccount');
 Route::get('/register', [UserController::class, 'showRegisterPage'])->name('showRegister');
 Route::post('/register', [UserController::class, 'store'])->name('registerAccount');
-Route::post('/upload-profile-picture', [UserController::class, 'uploadProfilePicture'])->name('uploadProfilePicture');
-Route::get('/todolist', [ToDoListController::class, 'show'])->name('ToDoList');
-Route::get('/todolist/history', [ToDoListController::class, 'showHistory'])->name('ToDoListHistory');
-Route::post('/addtodolist', [ToDoListController::class, 'store'])->name('AddToDoList');
+
+// Authenticated Routes (Protected by 'auth' middleware)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/ProfilePage', [UserController::class, 'profile'])->name('ProfilePage');
+    Route::post('/logout', [UserController::class, 'accountLogout'])->name('logout');
+    Route::post('/upload-profile-picture', [UserController::class, 'uploadProfilePicture'])->name('uploadProfilePicture');
+    Route::get('/todolist', [ToDoListController::class, 'show'])->name('ToDoList');
+    Route::get('/todolist/history', [ToDoListController::class, 'showHistory'])->name('ToDoListHistory');
+    Route::patch('/to-do-lists/{id}/update-progress', [ToDoListController::class, 'updateProgress'])->name('updateProgress');
+    Route::post('/addtodolist', [ToDoListController::class, 'store'])->name('AddToDoList');
+});
