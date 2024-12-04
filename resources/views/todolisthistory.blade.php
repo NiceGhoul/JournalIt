fb477af8967fcdd3a24039256706ab1bac8fda98
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2024-12-04 09:51:11 +0700

    ui

--- resources/views/todolisthistory.blade.php
+++ resources/views/todolisthistory.blade.php
@@ -23,7 +23,7 @@
             <h1 class="text-2xl font-bold text-white ">To Do List History</h1>
             <div class="flex space-x-4">
                 <div class="container flex justify-end items-center gap-4 ">
-                    <button type="button" onclick="window.history.back();"
+                    <button type="button" onclick="window.location.href='/todolist';"
                         class="flex items-center bg-gray-600 text-white py-2 px-4 rounded-lg shadow hover:bg-gray-700 hover:shadow-lg transition duration-200">
                         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 2a8 8 0 100 16 8 8 0 000-16zm-1 11H7v-2h2v2zm0-4H7V7h2v2z" />
@@ -38,7 +38,7 @@
             <div class="flex flex-col gap-4 mt-5">
                 @forelse ($toDoLists as $todo)
                     <div
-                        class="bg-customBlue shadow-md rounded-lg p-4 flex flex-row items-center transition-shadow duration-300 hover:shadow-lg border-3 border-black">
+                        class="bg-customBlue shadow-md rounded-lg p-4 flex flex-row items-center transition-shadow transition-transform ease-in-out  duration-300 hover:shadow-lg hover:-translate-y-1 border-3 border-black">
                         <!-- Image -->
                         @if ($todo->logo)
                             <img src="{{ asset($todo->logo) }}" class="h-24 w-24 object-cover rounded-md mr-4"
